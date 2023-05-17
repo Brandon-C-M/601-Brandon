ea9dc76f645ca048682b8775095550024397344a
Brando-C-M <brandonlrv123@gmail.com>
2023-05-16 21:49:50 -0500

    Se agrega el programa holaMundo

---
+++ 1 holaMundo/src/pkg1/holamundo/HolaMundo.java
@@ -0,0 +1,14 @@
+
+package pkg1.holamundo;
+
+
+public class HolaMundo {
+
+   
+    public static void main(String[] args) {
+      
+        System.out.println("Hola mundo");
+        
+    }
+    
+}
