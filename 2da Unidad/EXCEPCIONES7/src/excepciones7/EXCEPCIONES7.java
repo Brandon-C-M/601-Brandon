ae0d82e47f203eb34bb7f06ec8792b1d7b19fe35
Brando-C-M <brandonlrv123@gmail.com>
2023-07-04 23:07:09 -0600

    Se agregran programas EXCEPCIONES

---
+++ 2da Unidad/EXCEPCIONES7/src/excepciones7/EXCEPCIONES7.java
@@ -0,0 +1,23 @@
+
+package excepciones7;
+
+
+public class EXCEPCIONES7 {
+
+    
+     public static void main(String[] args) {
+         try {
+            String str = "Hola";
+            Object obj = str;
+            
+            Integer number = (Integer) obj; // Genera la excepción ClassCastException
+            
+            System.out.println("Número obtenido: " + number);
+        } catch (ClassCastException e) {
+            System.out.println("¡Se produjo una excepción!");
+            System.out.println("La conversión entre tipos es inválida.");
+            System.out.println("Mensaje: " + e.getMessage());
+        }
+    }
+    
+}
---
+++ 2da Unidad/EXEPCIONES1/src/exepciones1/EXEPCIONES1.java
@@ -0,0 +1,24 @@
+
+package exepciones1;
+
+import javax.swing.JOptionPane;
+
+
+public class EXEPCIONES1 {
+
+  
+    
+   public static void main(String[] args) {
+        
+        int x,y;
+        
+        try {
+        x = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese numero 1:", "Solicito datos", 3));
+    }catch (NumberFormatException n){
+        
+        System.out.println (n.getMessage() + " NO ES UN NUMERO ENTERO");
+    }
+        
+    }
+    
+}
